e4b7332d32a981b483d3ce1ffcea68ebc252b931
Rostyk Boiko <rosstyk@gmail.com>
2018-01-28 11:03:28 +0200

    Trains

--- Lesson1/src/com/epam/Main.java
+++ Lesson1/src/com/epam/Main.java
@@ -4,17 +4,10 @@ import com.epam.generics.MainGeneric;
 import com.epam.lesson1.Lesson1;
 import com.epam.trains.Lesson2;
 import com.epam.strings.Lesson5Main;
-import com.epam.generics.Generics;
-
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-
-//        Lesson5Main lesson3Main;
-//        lesson3Main = new Lesson5Main();
-//        lesson3Main.run();
-
         lessonChooser();
     }
 
@@ -22,27 +15,25 @@ public class Main {
         Scanner scanner = new Scanner(System.in);
         System.out.print("Lesson 1, Lesson 2. Lesson 3. Lesson 4. \nEnter chosen option: ");
         int option = scanner.nextInt();
-        switch (option) {
-            case 1:
-                Lesson1 lessonTask1 = new Lesson1();
-                lessonTask1.run();
-                break;
-            case 2:
-                Lesson2 lessonTask2 = new Lesson2();
-                lessonTask2.run();
-                break;
-            case 3:
-                Lesson5Main lesson5Main = new Lesson5Main();
-                lesson5Main.run();
-                break;
-            case 4:
-                MainGeneric generic = new MainGeneric();
-                generic.run();
-                break;
-            default:
-                System.out.println("Error: Wrong value");
-                lessonChooser();
-                break;
+        if (option == 1) {
+            Lesson1 lessonTask1 = new Lesson1();
+            lessonTask1.run();
+
+        } else if (option == 2) {
+            Lesson2 lessonTask2 = new Lesson2();
+            lessonTask2.run();
+
+        } else if (option == 3) {
+            Lesson5Main lesson5Main = new Lesson5Main();
+            lesson5Main.run();
+
+        } else if (option == 4) {
+            MainGeneric generic = new MainGeneric();
+            generic.run();
+
+        } else {
+            System.out.println("Error: Wrong value");
+            lessonChooser();
         }
     }
 }
--- Lesson1/src/com/epam/trains/Ticket.java
+++ Lesson1/src/com/epam/trains/Ticket.java
@@ -1,16 +1,22 @@
 package com.epam.trains;
 
-public class Tickets {
+public class Ticket {
     private int ticketNumber;
     private int train;
     private int carriage;
     private int seat;
+    private Station fromStation;
+    private Station toStation;
 
-    public Tickets(int ticketNumber, int train, int carriage, int seat) {
+    public Ticket(){}
+
+    public Ticket(int ticketNumber, int train, int carriage, int seat, Station fromStation, Station toStation) {
         this.ticketNumber = ticketNumber;
         this.train = train;
         this.carriage = carriage;
         this.seat = seat;
+        this.fromStation = fromStation;
+        this.toStation = toStation;
     }
 
     public int getTicketNumber() {
@@ -43,5 +49,21 @@ public class Tickets {
 
     public void setSeat(int seat) {
         this.seat = seat;
+    }
+
+    public Station getFromStation() {
+        return fromStation;
+    }
+
+    public void setFromStation(Station fromStation) {
+        this.fromStation = fromStation;
+    }
+
+    public Station getToStation() {
+        return toStation;
+    }
+
+    public void setToStation(Station toStation) {
+        this.toStation = toStation;
     }
 }
