4cdc2a9761b54520efd78b1a320947b61e1694c1
Rostyk Boiko <rosstyk@gmail.com>
2018-02-25 21:42:39 +0200

    OpenClose Refactor

--- Lesson1/src/com/epam/refactorRev2/CommandService.java
+++ Lesson1/src/com/epam/refactorRev2/CommandService.java
@@ -4,12 +4,12 @@ public class CommandService implements IHandler {
     public void Add(String commandName, ICommandHandler handler) {
     }
 
-    public void Handle(Context context, Character[] line) {
+    public void Handle(Context context, Character line) {
         // TODO
         // first word on the line is the command, all other words are arguments.
         // split the string properly
 
-        // then find the corrext command handler and invoke it.
+        // then find the correct command handler and invoke it.
         // take the result and add it to the <code>IProcessContext</code>
     }
 
