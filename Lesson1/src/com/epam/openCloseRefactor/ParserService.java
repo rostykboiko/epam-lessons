1b7eba50b06f546d483e2dd76cee73245b5d73df
Rostyk Boiko <rosstyk@gmail.com>
2018-03-09 15:40:48 +0200

    CleanUp

--- Lesson1/src/com/epam/openCloseRefactor/ParserService.java
+++ Lesson1/src/com/epam/openCloseRefactor/ParserService.java
@@ -1,17 +1,17 @@
-package com.epam.openCloseRefactor;
-
-public class ParserService implements LineHandler {
-    private CommandService commandSerivce;
-    @Override
-    public void handleScript(String line) {
-        if (line.substring(0, 1).equals("$")) {
-            // handleScript the entire "line" as a variable,
-            // i.e. add it to a collection of KeyValuePair.
-            AddToVariables(line);
-        } else if (line.substring(0, 1).equals("!")) {
-            commandSerivce.commandHandler(line);
-        } else {
-            scope.append();
-        }
-    }
-}
+//package com.epam.openCloseRefactor;
+//
+//public class ParserService implements LineHandler {
+//    private CommandService commandSerivce;
+//    @Override
+//    public void handleScript(String line) {
+//        if (line.substring(0, 1).equals("$")) {
+//            // handleScript the entire "line" as a variable,
+//            // i.e. add it to a collection of KeyValuePair.
+//            AddToVariables(line);
+//        } else if (line.substring(0, 1).equals("!")) {
+//            commandSerivce.commandHandler(line);
+//        } else {
+//            scope.append();
+//        }
+//    }
+//}
